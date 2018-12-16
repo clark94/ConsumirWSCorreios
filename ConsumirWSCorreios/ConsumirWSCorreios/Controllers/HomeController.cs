91a9a6468c9a5127b68061c0865abad244f4a29b
clark94 <gomes.lucas@outlook.com.br>
2018-12-15 22:34:27 -0200

    Commit final onde foi feita a Ui e desenvolvido o Software

---
+++ ConsumirWSCorreios/ConsumirWSCorreios/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ConsumirWSCorreios.Correios;
+using ConsumirWSCorreios.Models;
+
+namespace ConsumirWSCorreios.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       public PartialViewResult ObterCep(string cep)
+        {
+
+            var cli = new Cliente();
+
+            try
+            {
+
+                var ConsultaCorreios = new AtendeClienteClient().consultaCEP(cep);
+
+                //Binding with my Model     
+                cli.CEP = ConsultaCorreios.cep;
+                cli.Bairro = ConsultaCorreios.bairro;
+                cli.Cidade = ConsultaCorreios.cidade;
+                cli.Endereco = ConsultaCorreios.end;
+                cli.UF = ConsultaCorreios.uf;
+                
+
+
+            }
+
+            catch (System.Exception a)
+            {
+
+                ViewData["Erro"] = a.Message;
+
+                return PartialView("Error");
+              
+            }
+       
+            //Chamei o método principal da Classe do nosso cliente terceiro e bem como chamei o método consultaCep();
+            return PartialView(cli);
+        }
+
+    }
+}
---
+++ ConsumirWSCorreios/ConsumirWSCorreios/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConsumirWSCorreios.Models
+{
+    public class Cliente
+    {
+        public String Bairro { get; set;}
+        public String CEP { get; set;}
+        public String Cidade { get; set;}
+        public String Endereco { get; set;}
+        public String UF { get; set;}
+
+    }
+}
